049ecf3ad3960c6f835aabed5de8140e194a09db
mr-kremez <mr.kremez@gmail.com>
2014-12-24 09:28:07 +0300

    Make changes in tests

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('Home')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About Us') }
+    it { should have_title(full_title('About')) }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+end
