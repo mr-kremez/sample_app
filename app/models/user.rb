431512b3e51ddfb4003ddfe4bc7a43e2d873f210
mr-kremez <mr.kremez@gmail.com>
2015-01-10 11:49:32 +0300

    make user-microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]*[a-z\d\-]\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
@@ -16,6 +18,11 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
   end
 
   private
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,11 +2,16 @@ FactoryGirl.define do
   factory :user do
     sequence(:name)  { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com"}
-    password "foobar"
-    password_confirmation "foobar"
+    password 'foobar'
+    password_confirmation 'foobar'
   end
 
   factory :admin do
     admin true
+  end
+
+  factory :micropost do
+    content 'Lorem ipsum'
+    user
   end
 end
